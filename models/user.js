9a7f88cf40080cc637319c45ffcfe758bd6f7cdf
Gohar Ali <gorry445@gmail.com>
2025-07-08 10:30:00 +0500

    Create User model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    googleId: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+    }
+  }, {
+    timestamps: true,
+    underscored: true,
+  });
+
+  return User;
+};
